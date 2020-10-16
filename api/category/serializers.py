d915d55d2cdf007af07320d01204814586f25b43
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-16 17:20:44 +0600

    create data serializers and viewsets

--- api/category/serializers.py
+++ api/category/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+
+from .models import Category
+
+
+class CategorySerializer(serializers.Serializer):
+    class Meta:
+        model = Category
+        field = ('name', 'description')
