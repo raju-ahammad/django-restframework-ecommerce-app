fe0f3e6e1155d52cbff9a54127ab881f04aeb43d
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-17 02:12:57 +0600

    create custom user

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50, default='Anonymous')
+    email = models.EmailField(max_length=254, unique=True)
+    
+    username = None
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    
+    
+    session_token = models.CharField(max_length=20, default=0)
+    
+    careated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- ecom/settings.py
+++ ecom/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
      'rest_framework.authtoken',
      'api',
      'api.category',
-     'api.product'
+     'api.product',
+     'api.user',
 ]
 
 MIDDLEWARE = [
