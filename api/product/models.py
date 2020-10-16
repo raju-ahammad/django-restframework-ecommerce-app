8665c943b1e349b87bd64b2d8d63fdb812adf7b7
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-16 18:59:47 +0600

    create product models

--- api/product/models.py
+++ api/product/models.py
@@ -1,3 +1,19 @@
+from api.category.models import Category
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    price = models.CharField(max_length=50)
+    stock = models.CharField(max_length=50)
+    is_activae = models.BooleanField(default=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    careated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+     
--- ecom/settings.py
+++ ecom/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
      'rest_framework',
      'rest_framework.authtoken',
      'api',
-     'api.category'
+     'api.category',
+     'api.product'
 ]
 
 MIDDLEWARE = [
