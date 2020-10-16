7178bf7d6e2b6cf338b75dbcdcaa22cbdecd2577
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-16 22:55:44 +0600

    create product viewsets

--- api/product/views.py
+++ api/product/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializers
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializers
