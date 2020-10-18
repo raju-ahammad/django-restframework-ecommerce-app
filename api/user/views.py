0a8d1552fe257aed74f9929e7111a3dd7dfe96e8
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-18 16:05:00 +0600

    create session tokoen function

--- api/user/views.py
+++ api/user/views.py
@@ -1,3 +1,9 @@
+# Create your views here.
+import random
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def generate_session_token(length=10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)]+ [str(i) for i in range(10)]) for _ in range(length))
+    
