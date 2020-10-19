f7b0665e8b0b3332f3c512fed813c03ab7615dc3
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-20 01:34:30 +0600

    create order view set

--- api/order/serializers.py
+++ api/order/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Order
 
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
         fields = ('user')
--- api/order/views.py
+++ api/order/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets
+
+from .models import Order
+from .serializers import OrderSerializer
 
 # Create your views here.
+
+def validate_user_session(id, token):
+    userModel = get_user_model()
+    
+    try:
+        user = userModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except userModel.DoesNotExist:
+        return False
