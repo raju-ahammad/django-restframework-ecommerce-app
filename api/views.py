bd5209bfe924d161059267774e65852fb10ba98e
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-23 19:06:42 +0600

    update home page

--- api/views.py
+++ api/views.py
@@ -1,6 +1,12 @@
-from django.http import JsonResponse
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+from django.shortcuts import render
+
 
 def home(request):
     return JsonResponse({'info': 'django course ', "name": "Raju"})
+
+def homePage(request):
+    html = "<h1>This page have nothing</h1>"
+    return HttpResponse(html)
+    
--- ecom/urls.py
+++ ecom/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from api.views import homePage
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -22,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    path('', homePage, name="homePage")
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
